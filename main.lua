6d7250ef8ad60573de0a27a125e23784f95c7810
CubeXPKidZ <neelsingh341@gmail.com>
2026-06-13 06:05:49 +1000

    Add CubeXPKidZ GUI with various buttons

---
+++ main.lua
@@ -0,0 +1,329 @@
+local NewInstance = Instance.new('ScreenGui')
+NewInstance.Name = 'CubeXPKidZGUI'
+local ChildInstance = (function()
+local NewInstance = Instance.new('Frame')
+NewInstance.Name = 'Frame'
+NewInstance.Size = UDim2.new({0, 692}, {0, 387})
+NewInstance.Position = UDim2.new({0.109629191, 0}, {0.0519829951, 0})
+NewInstance.BackgroundColor3 = Color3.new(0.784314, 0, 0)
+NewInstance.Visible = true
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextLabel')
+NewInstance.Name = 'TextLabel'
+NewInstance.Size = UDim2.new({0, 473}, {0, 79})
+NewInstance.Position = UDim2.new({0.166574866, 0}, {-0.0137509583, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'CubeXPKidZGUI'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextButton')
+NewInstance.Name = 'c00lFlyButton'
+NewInstance.Size = UDim2.new({0, 70}, {0, 45})
+NewInstance.Position = UDim2.new({0.0093625905, 0}, {0.832784891, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'FLING URSELF'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextButton')
+NewInstance.Name = 'WalkSpeedButton'
+NewInstance.Size = UDim2.new({0, 82}, {0, 50})
+NewInstance.Position = UDim2.new({0.0114065185, 0}, {0.188933536, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'WALKSPEED'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextButton')
+NewInstance.Name = 'JumpPowerButton'
+NewInstance.Size = UDim2.new({0, 86}, {0, 45})
+NewInstance.Position = UDim2.new({0.0102924947, 0}, {0.269584715, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'JUMPPOWER'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextButton')
+NewInstance.Name = 'SitButton'
+NewInstance.Size = UDim2.new({0, 70}, {0, 45})
+NewInstance.Position = UDim2.new({0.00442681275, 0}, {0.340554118, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'Sit'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextButton')
+NewInstance.Name = 'LayDownButton'
+NewInstance.Size = UDim2.new({0, 110}, {0, 26})
+NewInstance.Position = UDim2.new({0.089292407, 0}, {0.364468753, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'Lay Down'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextButton')
+NewInstance.Name = 'TPButton'
+NewInstance.Size = UDim2.new({0, 149}, {0, 45})
+NewInstance.Position = UDim2.new({0.011341867, 0}, {0.458056033, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'TP TO MOUSE'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextLabel')
+NewInstance.Name = 'TextLabel'
+NewInstance.Size = UDim2.new({0, 209}, {0, 25})
+NewInstance.Position = UDim2.new({0.0198493991, 0}, {0.15317826, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'https://discord.gg/Cc3PZ4679K'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextButton')
+NewInstance.Name = 'InfiniteJumpButton'
+NewInstance.Size = UDim2.new({0, 112}, {0, 26})
+NewInstance.Position = UDim2.new({0.12770848, 0}, {0.821795404, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'INFINITE JUMP'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextBox')
+NewInstance.Name = 'WalkSpeedBox'
+NewInstance.Size = UDim2.new({0, 157}, {0, 24})
+NewInstance.Position = UDim2.new({0.1452007, 0}, {0.224553078, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'WALKSPEED: (default is 16)'
+NewInstance.Visible = true
+NewInstance.PlaceholderText = 'WALKSPEED:'
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextBox')
+NewInstance.Name = 'JumpPowerBox'
+NewInstance.Size = UDim2.new({0, 173}, {0, 29})
+NewInstance.Position = UDim2.new({0.134569988, 0}, {0.288874507, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'JUMPPOWER (default is 50'
+NewInstance.Visible = true
+NewInstance.PlaceholderText = 'JUMPOWER:'
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextLabel')
+NewInstance.Name = 'TextLabel'
+NewInstance.Size = UDim2.new({0, 250}, {0, 31})
+NewInstance.Position = UDim2.new({0.556781471, 0}, {0.144449711, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'made by @cubexpkidz on discord'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextLabel')
+NewInstance.Name = 'TextLabel'
+NewInstance.Size = UDim2.new({0, 236}, {0, 55})
+NewInstance.Position = UDim2.new({0.60059303, 0}, {0.223781437, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = '!THESE COMMANDS SUPPORT R6 AND R15!'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextLabel')
+NewInstance.Name = 'TextLabel'
+NewInstance.Size = UDim2.new({0, 236}, {0, 55})
+NewInstance.Position = UDim2.new({0.581089497, 0}, {0.381103784, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = '!REPORT ERRORS ON MY DISCORD SERVER ILL TRY TO FIX IT! https://discord.gg/Cc3PZ4679K'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextButton')
+NewInstance.Name = 'StandButton'
+NewInstance.Size = UDim2.new({0, 139}, {0, 31})
+NewInstance.Position = UDim2.new({0.225626156, 0}, {0.363997817, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'STAND UP'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextLabel')
+NewInstance.Name = 'TextLabel'
+NewInstance.Size = UDim2.new({0, 236}, {0, 55})
+NewInstance.Position = UDim2.new({0.581089377, 0}, {0.518197715, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'MORE SCRIPTS COMING SOON https://discord.gg/Cc3PZ4679K'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextLabel')
+NewInstance.Name = 'TextLabel'
+NewInstance.Size = UDim2.new({0, 236}, {0, 77})
+NewInstance.Position = UDim2.new({0.581089377, 0}, {0.660291612, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'IF THERE IS A PROBLEM WITH THE GUI LET ME KNOW IN CHANNEL #cubexpkidz-gui ON MY DISCORD SERVER'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextLabel')
+NewInstance.Name = 'TextLabel'
+NewInstance.Size = UDim2.new({0, 236}, {0, 55})
+NewInstance.Position = UDim2.new({0.567145109, 0}, {0.821941137, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'https://discord.gg/Cc3PZ4679K'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextButton')
+NewInstance.Name = 'UnInfiniteJumpButton'
+NewInstance.Size = UDim2.new({0, 112}, {0, 26})
+NewInstance.Position = UDim2.new({0.126726091, 0}, {0.896865189, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'UNINFINITE JUMP'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextButton')
+NewInstance.Name = 'ESPButton'
+NewInstance.Size = UDim2.new({0, 77}, {0, 41})
+NewInstance.Position = UDim2.new({0.257772446, 0}, {0.448193848, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'ESP (havent tested)'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextButton')
+NewInstance.Name = 'UnESPButton'
+NewInstance.Size = UDim2.new({0, 72}, {0, 37})
+NewInstance.Position = UDim2.new({0.410110831, 0}, {0.443420619, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'UNESP'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextButton')
+NewInstance.Name = 'UnTPButton'
+NewInstance.Size = UDim2.new({0, 156}, {0, 37})
+NewInstance.Position = UDim2.new({0.00551455282, 0}, {0.562907457, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'REMOVE TP TO MOUSE'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextButton')
+NewInstance.Name = 'RealFlyButton'
+NewInstance.Size = UDim2.new({0, 70}, {0, 45})
+NewInstance.Position = UDim2.new({0.0117362803, 0}, {0.633469701, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'real fly'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextButton')
+NewInstance.Name = 'RealUnFlyButton'
+NewInstance.Size = UDim2.new({0, 84}, {0, 34})
+NewInstance.Position = UDim2.new({0.128020972, 0}, {0.64583391, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'real unfly'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextBox')
+NewInstance.Name = 'RealFlyBox'
+NewInstance.Size = UDim2.new({0, 139}, {0, 30})
+NewInstance.Position = UDim2.new({0.00551455282, 0}, {0.733689189, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = ''
+NewInstance.Visible = true
+NewInstance.PlaceholderText = 'Real Fly SPEED'
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+local ChildInstance = (function()
+local NewInstance = Instance.new('TextLabel')
+NewInstance.Name = 'TextLabel'
+NewInstance.Size = UDim2.new({0, 236}, {0, 55})
+NewInstance.Position = UDim2.new({0.230439901, 0}, {0.715997994, 0})
+NewInstance.BackgroundColor3 = Color3.new(1, 1, 1)
+NewInstance.TextColor3 = Color3.new(1, 1, 1)
+NewInstance.Text = 'HOLD CTRL (left or right) AND LEFT CLICK = TP RELEASE CTRL = NORMAL CLICKING'
+NewInstance.Visible = true
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+return NewInstance
+end)()
+ChildInstance.Parent = NewInstance
+ 
+NewInstance.Parent = game:GetService('CoreGui') or game:GetService('Players').LocalPlayer:WaitForChild('PlayerGui')
